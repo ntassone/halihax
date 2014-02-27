e23df0843d120a9d2c44d16d489e41f79c90232f
Nicholas Tassone <ntassone@gmail.com>
2014-02-27 09:46:54 -0400

    created website

---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+var header = $('header'),
+    header_height = header.height();
+
+function shrinkHeader() {
+
+  var scrolled = -($(this).scrollTop() - header_height);
+
+  if ($(this).scrollTop() <= header_height / 1.4) {
+    header.css({
+      height: scrolled
+    });
+  }
+}
+
+$(window).scroll(function(){
+  shrinkHeader();
+});
